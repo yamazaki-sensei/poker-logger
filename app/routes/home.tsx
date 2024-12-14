35d09978487018bb8a807965148a50398f88188c
hira <almichest.free@gmail.com>
2024-12-14 09:56:00 +0900

    biomeをセットアップ

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,6 @@
-import type { Route } from "./+types/home";
 import { Tabs } from "@radix-ui/themes";
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
