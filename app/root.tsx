222b1b35ea62d8585a1b736b0d324c58d49e9a12
hira <almichest.free@gmail.com>
2024-12-15 10:45:32 +0900

    カスタムフォントは必要になったら入れる

--- app/root.tsx
+++ app/root.tsx
@@ -13,16 +13,6 @@ import stylesheet from "./app.css?url";
 import { GameSettingsFrame } from "./GameSettingsFrame";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
   { rel: "stylesheet", href: stylesheet },
 ];
 
